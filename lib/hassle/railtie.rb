5d8c80c7072486f0b0f7d9722ee625b25955fc77
Jay Boice <jboice@gmail.com>
2011-04-01 11:36:16 -0400

    Hassle on edge

--- lib/hassle/railtie.rb
+++ lib/hassle/railtie.rb
@@ -7,7 +7,7 @@ module HassleRails
       require 'sass'
       require 'sass/plugin'
 
-      if (defined?(Rails.env) && (Rails.env.staging? || Rails.env.production?))
+      if (defined?(Rails.env) && (Rails.env.staging? || Rails.env.production? || Rails.env.edge? || Rails.env.qa?))
         if defined?(Rails.configuration) && defined?(Rails.configuration.middleware)
           Rails.configuration.middleware.use(Hassle)
         elsif defined?(ActionController::Dispatcher) && defined?(ActionController::Dispatcher.middleware)
